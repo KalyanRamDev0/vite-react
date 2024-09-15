c98e0486eb11ba16d907446f6708af73b8ee71f1
KalyanRamDev0 <kalyanram044@gmail.com>
2024-09-15 19:43:36 +0530

    task1

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
+import CustomUpdateForm from './components/updateForm/updateForm'
 
-//import LoginComponent from "./components/login";
-import CoustomButton from "./components/button.jsx";
-import Greeting from "./components/greeting/greeting.jsx";
-import style from "./components/greeting/greeging.module.css"
-import Customcard from "./components/bootstrap/custom-card.jsx";
-import { employeeDetails } from "./components/data/employeedata.js";
-import FirstComponent from "./components/bootstrap/first-component.jsx";
-import BasicExample from "./components/bootstrap/navbar.jsx";
+function App() {
 
-
-
-
-
-
-const App=()=>{
-
- return(
-  <div >
-   {/* hello kalyan
-   <div className={style.card}> hello kalyan</div>
-   <CoustomButton/>
-   <Greeting/>
-   */}
-   {/* {
-    employeeDetails.map(each=>{
-return(
+  return (
     <>
-     <Customcard     title={each.name} text={each.designation}/>
+      <CustomUpdateForm/>
     </>
-)
-
-
-    })
-   } */}
-  {/* <Customcard     title={"john"} text={"software dev"}/> */}
-  <FirstComponent></FirstComponent>
-  <BasicExample></BasicExample>
-  </div>
- ) 
+  )
 }
-export default App;
+
+export default App
